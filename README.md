ba97870ddf87f1458cd4d40bc853d6c1e34694a1
Fatih Erikli <fatiherikli@gmail.com>
2013-02-24 21:38:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Backbone.EditInPlaceView
+
+An Edit In Place widget built with Backbone.js
+
+#### Demo & Examples:
+
+<http://fatiherikli.github.com/backbone-editinplace/>
---
+++ src/backbone.editinplace.js
@@ -0,0 +1,51 @@
+var EditInPlaceForm = Backbone.View.extend({
+    tagName: "form",
+
+    events: {
+        "submit": "save"
+    },
+
+    initialize: function (options) {
+        _.extend(this, options);
+    },
+
+    render: function () {
+        this.$el.html($("<input>", {
+            value: this.model.get(this.attribute)
+        }));
+        return this;
+    },
+
+    save: function () {
+        this.model.set(this.attribute, this.$el.find("input").val());
+        return false;
+    }
+});
+
+var EditInPlaceView = Backbone.View.extend({
+
+    attribute: "text",
+
+    initialize: function (options) {
+        _.extend(this, options);
+        this.model.on("change", this.render, this);
+    },
+
+    events: {
+        "dblclick": "edit"
+    },
+
+    render: function () {
+        this.$el.html(this.model.get(this.attribute));
+        return this;
+    },
+
+    edit: function () {
+        this.$el.html(new EditInPlaceForm({
+            model: this.model,
+            attribute: this.attribute
+        }).render().el);
+        this.$el.find("input").select();
+    }
+
+});
